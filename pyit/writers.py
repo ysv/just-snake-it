efc46edd8ba95fa2fa3c52648ffec2ac69f0b268
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2018-12-11 23:48:58 +0200

    Move config file to project root. Fix bin/pyit

--- pyit/writers.py
+++ pyit/writers.py
@@ -1,5 +1,6 @@
 def print_inspection_files(runners):
     # TODO: Beautiful output for inspection files.
+    print('Files for inspection:')
     for r in runners:
         for f in r.inspection_files:
             print(f)
