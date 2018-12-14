27673a94646850c12f6e944275095d0beb7c4ca3
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2018-12-14 12:47:08 +0200

    Add BlankLinesCop specs. Fix other for BlankLinesCop convention

--- test/blank_lines.py
+++ test/blank_lines.py
@@ -1,4 +1,6 @@
 
+
+
 def sing_blank_line():
     print('single!')
 
@@ -19,3 +21,14 @@ def nested_two_blank():
 
     a = 1
     print(a)
+
+def top_level_def():
+    def nested_level_def():
+        pass
+    return nested_level_def()
+
+class TopLevelClass:
+    class NestedLevelClass:
+        def nested_nested_level_def(self):
+            def nested_nested_nested_level_def():
+                pass
---
+++ test/blank_lines2.py
@@ -0,0 +1,14 @@
+def its_okay_without_blank():
+    pass
+def bad_bad_indent():
+    pass
+class BadBadIndent:
+
+
+    def too_bad(self):
+        pass
+
+    a = 90
+
+
+    dont_use_two = 88
