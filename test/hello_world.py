5d9b941a77a65eafc98663c8ac70061924fcebc8
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2018-12-14 12:47:08 +0200

    Add BlankLinesCop specs. Fix other for BlankLinesCop convention

--- test/hello_world.py
+++ test/hello_world.py
@@ -1,6 +1,17 @@
 
+
+# KEk
 def hello():
     print('Hello World!')
+    a = 0
+    return a + 9
 
+
+@classmethod
 class Test:
-    pass
+    b = 0
+
+    def __init__(self):
+        pass
+
+    c = 1
